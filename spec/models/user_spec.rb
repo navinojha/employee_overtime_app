b7c606046fa0ad268b2ad891ea95298afc028dfc
Navin Ojha <navinojha1996@gmail.com>
2021-02-02 01:48:35 +0530

    Added User Validations with TDD

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Creation" do 
+    before do
+      @user = User.create(email: "navin@example.com", password:"123456", password_confirmation:"123456", first_name: "Navin", last_name: "Ojha")
+    end
+
+    it "can be created" do
+      expect(@user).to be_valid
+    end 
+
+    it "cannot be created without first_name, last_name" do
+      @user.first_name = nil
+      @user.last_name = nil
+      expect(@user).to_not be_valid
+    end
+  end
 end
