2d7978d9c839e604037913a7f846a955da6a9411
Navin Ojha <navinojha1996@gmail.com>
2021-02-23 01:01:52 +0530

    Added Specs for Post creationa and deletion

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Creation of Posts" do
+    before do
+      @post = Post.create(date: Date.today, rationale: "Anything can be posted")
+    end
+
+    it "can be created" do
+      expect(@post).to be_valid
+    end
+
+    it "cannot be created without date and rationale" do
+      @post.date = nil
+      @post.rationale = nil
+      expect(@post).not_to be_valid
+    end
+  end
 end
